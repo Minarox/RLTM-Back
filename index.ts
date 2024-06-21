e7d14b4318de1137cca998f29d52b37f81b7c5f4
Minarox <contact@minarox.fr>
2024-06-21 10:26:27 +0000

    feat: Load .env variables

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
+import dotenv from "dotenv";
+
+// Load env variables
+const envCheck = dotenv.config();
+if (envCheck.error) {
+  throw new Error('Unable to find or read .env file: ' + envCheck.error)
+}
+
 console.log("Hello World!");
