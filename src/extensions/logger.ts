d0a611bbb7e6c9d925c18115f08a085675873f51
Mathis S.M. <contact@minarox.fr>
2024-07-13 20:15:50 +0200

    refactor: log format in console

--- src/extensions/logger.ts
+++ src/extensions/logger.ts
@@ -1,6 +1,7 @@
 import {createPinoLogger, pino} from "@bogeychan/elysia-logger";
 import pinopretty from "pino-pretty";
 import fs from "node:fs";
+import { app } from "../index.ts";
 
 export default createPinoLogger({
     level: "debug",
@@ -8,8 +9,10 @@ export default createPinoLogger({
         {level: 'debug', stream: pinopretty({
                 singleLine: true,
                 hideObject: true,
-                messageFormat: (log) => {
-                    return `${JSON.stringify(log)}\n`
+                messageFormat: (log): string => {
+                    const origin: string = app?.server?.url?.origin || '';
+                    const request: any = log["request"];
+                    return `${request.method} ${request.url.slice(origin.length)} (${parseFloat(log["responseTime"] as string).toFixed(2)}ms)\n`;
                 }
             })},
         {level: 'info', stream: fs.createWriteStream('logs/debug.log')},
