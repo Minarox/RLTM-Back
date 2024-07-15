dbbdfdddd7567f1f02e9ef7ecac3c4d2cd39fed6
Mathis S.M. <contact@minarox.fr>
2024-07-15 20:03:59 +0200

    feat: Add car pictures

--- src/schemas/car.ts
+++ src/schemas/car.ts
@@ -4,6 +4,6 @@ import { sql } from "drizzle-orm";
 export const car: SQLiteTable = sqliteTable("car", {
     id: integer("id").primaryKey({ autoIncrement: true }),
     name: text("name").notNull(),
-    picture: text("picture").notNull(),
+    picture: blob("picture").notNull(),
     createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`)
 });
